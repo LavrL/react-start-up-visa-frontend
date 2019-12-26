c6a2983a0b0e9457ff00025a4d84296d884a1ef7
LavrL <llembersk@gmail.com>
2019-12-26 23:08:15 +0200

    fixed serialize-javascript issue

--- src/section/Advantages/Advantages.js
+++ src/section/Advantages/Advantages.js
@@ -5,6 +5,14 @@ const Advantages = () => {
     return (
         <div className="Advantages">
             <p className="Advantages__title">Что вы получаете?</p>
+            <ul className="Advantages__list">
+                <li className="Advantages__item">
+                    <div className="Advantages__item_title">Качественную и актуальную информацию</div>
+                    <p className="Advantages__item_text">Вся информация взята не просто из открытых источников,
+                    а проверена на себе. Инструкции пишут люди, которые сами переезжали по этим программам</p>
+                    <div className="Advantages__number-background">1</div>
+                </li>
+            </ul>
         </div>
     )
 }
