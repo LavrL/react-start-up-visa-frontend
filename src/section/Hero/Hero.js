46e1e598981b1c77c622636c29e62cb66279c564
LavrL <llembersk@gmail.com>
2019-12-27 21:52:40 +0200

    added arrow to bottom

--- src/section/Hero/Hero.js
+++ src/section/Hero/Hero.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Hero.css';
 import Button from '../../button';
-
+import arrowtobottom from '../../img/arrtobottom.png';
 
 const Hero = () => {
     return (
         <div>
-        <header className="Hero ">
-            <div className="Hero__container">
-                <p className="Hero__title">Инструкция по программе Startup Visa Испания</p>
-                <span className="Hero__description">2499 рублей за подробный и пошаговый
+            <header className="Hero ">
+                <div className="Hero__container">
+                    <p className="Hero__title">Инструкция по программе Startup Visa Испания</p>
+                    <span className="Hero__description">2499 рублей за подробный и пошаговый
                 план вашего переезда в Испанию по программе Startup Visa</span>
-                <div className="Buttons">
-                    <Button newClassName="Btn_blue" btnName="Купить инструкцию" />
-                    <Button newClassName="Btn_white" btnName="Позвоните мне" />
+                    <div className="Buttons">
+                        <Button newClassName="Btn_blue" btnName="Купить инструкцию" />
+                        <Button newClassName="Btn_white" btnName="Позвоните мне" />
+                    </div>
                 </div>
-            </div>
-        </header>
+                <div className="Hero__arrow bounce" >
+                    <img src={ arrowtobottom } alt='arrow'/>
+                </div>
+            </header>
         </div>
     )
 }
